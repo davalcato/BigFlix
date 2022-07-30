4c2ef7ff893b911bcaa260d00beb27ffa98b908b
davalcato <davalcato@yahoo.com>
2022-07-30 14:12:08 -0700

    first commit

---
+++ BigFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BigFlix
+//
+//  Created by Daval Cato on 7/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
