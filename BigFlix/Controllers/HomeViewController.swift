51ca104d2abe96a78b8e1b199bd77c1726626db7
davalcato <davalcato@yahoo.com>
2022-08-05 15:33:51 -0700

    registered cell to customize cell on the HomeViewController along with adding the tableview to view in the viewdidload.

--- BigFlix/Controllers/HomeViewController.swift
+++ BigFlix/Controllers/HomeViewController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let homeFeedTable: UITableView = {
+        
+        // Instantize the table here
+        let table = UITableView()
+        // registered cell to customize cell
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        // added tableview to view
+        view.addSubview(homeFeedTable)
+        
+        
     }
     
 
