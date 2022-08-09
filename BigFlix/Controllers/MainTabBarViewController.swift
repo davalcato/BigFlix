a3f75f0022031c0ccece279258caf43aea086569
davalcato <davalcato@yahoo.com>
2022-08-09 15:58:14 -0700

    created a ProfileViewController and the person.crop.circle

--- BigFlix/Controllers/MainTabBarViewController.swift
+++ BigFlix/Controllers/MainTabBarViewController.swift
@@ -17,22 +17,26 @@ class MainTabBarViewController: UITabBarController {
         let vc2 = UINavigationController(rootViewController: UpComingViewController())
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        let vc5 = UINavigationController(rootViewController: ProfileViewController())
+        
         
         // tabbar items
         vc1.tabBarItem.image = UIImage(systemName: "house")
         vc2.tabBarItem.image = UIImage(systemName: "play.circle")
         vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
         vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        vc5.tabBarItem.image = UIImage(systemName: "person.crop.circle")
         
         vc1.title = "Home"
         vc2.title = "Coming Some"
-        vc3.title = "Top Searches"
+        vc3.title = "Searches"
         vc4.title = "Downloads"
+        vc5.title = "Profile"
         
         tabBar.tintColor = .label
         
         // create array of vc
-        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: true)
         
     }
 
---
+++ BigFlix/Controllers/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  BigFlix
+//
+//  Created by Daval Cato on 8/9/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
