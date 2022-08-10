c60ebdc3d7a10e9b86467c090130d8b8a57eeb7c
davalcato <davalcato@yahoo.com>
2022-08-09 17:10:43 -0700

    registered the UICollectionViewCell on the CollectionViewTableViewCell and added the contentView.bounds

--- BigFlix/Views/CollectionViewTableViewCell.swift
+++ BigFlix/Views/CollectionViewTableViewCell.swift
@@ -10,14 +10,56 @@ import UIKit
 class CollectionViewTableViewCell: UITableViewCell {
 
     static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        // define layout
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        // register tableview
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        
+        return collectionView
+    }()
+    
     // intialize
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         // color
         contentView.backgroundColor = .systemPink
+        // add to content view
+        contentView.addSubview(collectionView)
+        
+        // conform to protocol
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // frame
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // collectioView the entire body of cell
+        collectionView.frame = contentView.bounds
+    }
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
 }
