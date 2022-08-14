29558cfd01bcd97b17c92d3dd55f714a92af39dd
davalcato <davalcato@yahoo.com>
2022-08-13 17:10:26 -0700

    added the fading out effect to the heroImage on the HomeViewController and then added the func addGradient. Then added the constraints bottom play.

--- BigFlix/Views/HeroHeaderUIVew.swift
+++ BigFlix/Views/HeroHeaderUIVew.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class HeroHeaderUIVew: UIView {
     
+    // add buttons
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        
+        // constraints for button
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     // implement image view
     private let heroImageView: UIImageView = {
         
@@ -18,12 +30,39 @@ class HeroHeaderUIVew: UIView {
         imageView.image = UIImage(named: "heroImage")
         return imageView
     }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        // frame for gradient layer
+        gradientLayer.frame = bounds
+        // add sublayer
+        layer.addSublayer(gradientLayer)
+    }
 
    // add frame
     override init(frame: CGRect) {
         super.init(frame: frame)
         // no constraints here
         addSubview(heroImageView)
+        // add gradient
+        addGradient()
+        addSubview(playButton)
+        // add another function
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)
+        ]
+        // activate constraints
+        NSLayoutConstraint.activate(playButtonConstraints)
     }
     
     override func layoutSubviews() {
