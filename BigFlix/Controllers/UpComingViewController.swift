bd03e52a0787ba72f6b112542e7bd13101f66310
davalcato <davalcato@yahoo.com>
2022-08-25 15:42:39 -0700

    called api and void memory leaks with weak in the fetchUpcoming on the UpComingViewController

--- BigFlix/Controllers/UpComingViewController.swift
+++ BigFlix/Controllers/UpComingViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class UpComingViewController: UIViewController {
     
+    // assign to a local variable switch result
+    private var titles: [Title] = [Title]()
+    
     // added new table
     private let upcomingTable: UITableView = {
         
@@ -30,24 +33,45 @@ class UpComingViewController: UIViewController {
         view.addSubview(upcomingTable)
         upcomingTable.delegate = self
         upcomingTable.dataSource = self
+        
+        // call the function
+        fetchUpcoming()
     }
     // add the tableview
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         upcomingTable.frame = view.bounds
-        
+    }
+    // fetch upcoming data
+    private func fetchUpcoming() {
+        // called api and avoid memory leaks with weak
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+                // retrieve titles
+            case .success(let titles):
+                // reference
+                self?.titles = titles
+                // put inside an async func for main thread
+                DispatchQueue.main.async {
+                    // reload the data
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
 extension UpComingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "unknown"
         return cell
     }
     
